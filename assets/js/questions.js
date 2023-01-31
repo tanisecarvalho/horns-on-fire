addeea5651c54bdfee834371f6e76fdfeb7bdb7d
Tanise Carvalho <carvalhotanise@gmail.com>
2023-01-31 13:03:04 +0000

    feat: add function start game and load question to script.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,13 @@
 // questions for the quiz
 const hardRock = [
   {
-    "question": "Who's GN'R lead singer?",
-    "opt1": "John Lennon",
-    "opt2": "Steven Tyler",
-    "opt3": "Axl Rose",
-    "opt4": "Jani Lane"
+    "question": "Who's the lead singer of the band Guns N' Roses?",
+    "options": ["John Lennon", "Steven Tyler", "Axl Rose", "Jani Lane"],
+    "correct": "Axl Rose"
   },
   {
     "question": "From which band is the song 'I Saw Red'?",
-    "opt1": "Warrant",
-    "opt2": "Stryper",
-    "opt3": "Faster Pussycat",
-    "opt4": "Winger"
+    "options": ["Warrant", "Stryper", "Faster Pussycat", "Winger"],
+    "correct": "Warrant"
   }
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("Connected");
+console.log(hardRock);
+
+function start() {
+  document.getElementById("start-game").style.display = "none";
+  document.getElementById("questions-game").style.display = "flex";
+
+  loadQuestion();
+}
+
+function loadQuestion() {
+  document.getElementById("question").innerHTML = hardRock[0].question;
+  let options = document.getElementById("options");
+  for (let opt of hardRock[0].options) {
+    let optButton = document.createElement("input");
+    optButton.type = "button";
+    optButton.value = opt;
+    options.appendChild(optButton);
+  }
+}
