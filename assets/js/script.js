998d5675e1033210e3607917d1e1a40699415c3c
Tanise Carvalho <carvalhotanise@gmail.com>
2023-02-01 11:10:16 +0000

    feat: add the function to go to the next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 console.log(hardRock);
+let currentQuestion = 0;
 
 function start() {
   document.getElementById("start-game").style.display = "none";
@@ -8,12 +9,18 @@ function start() {
 }
 
 function loadQuestion() {
-  document.getElementById("question").innerHTML = hardRock[0].question;
+  document.getElementById("question").innerHTML = hardRock[currentQuestion].question;
   let options = document.getElementById("options");
-  for (let opt of hardRock[0].options) {
+  options.innerHTML = "";
+  for (let opt of hardRock[currentQuestion].options) {
     let optButton = document.createElement("input");
     optButton.type = "button";
     optButton.value = opt;
     options.appendChild(optButton);
   }
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  loadQuestion();
 }
